a4dccb86ae7282add9ebe336a5a3a7e235dfa0d3
unknown <diloch111@gmail.com>
2023-07-30 01:03:01 +0100

    added computer options and win/loss variables to js

---
+++ script/rpc.js
@@ -0,0 +1,14 @@
+//create computer options based on "r" for rock, "p" for paper, "s" for scissors
+var computerOptions = ["r", "p", "s"];
+// console.log(computerOptions);
+
+// create variables for wins, losses and ties
+var myWins = 0;
+var myLosses = 0;
+var myTies = 0;
+
+// add a for loop 
+for(i = 0; i < 10; i++){
+    
+//create function to randomize computer options
+}
