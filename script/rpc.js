98207952fadf9f5e458f2442565288f5ae02ae53
unknown <diloch111@gmail.com>
2023-07-30 02:08:06 +0100

    added conditions and summary

--- script/rpc.js
+++ script/rpc.js
@@ -14,5 +14,34 @@ for(i = 0; i < 9; i++){
 var computerRndm =  computerOptions[Math.floor(Math.random() * computerOptions.length)];
 // alert(computerRndm);
 
+//user invitation to play
+var userOptions = prompt("select letters 'r' for rock, 'p' for paper and 's' for scissors");
+
+// add general condition limiting user input to "r", "p" and "s"
+if (userOptions === "r" || userOptions === "p" || userOptions === "s") {
+    alert("computer selected " + computerRndm);
+    
+    //add game conditional statements
+    if (userOptions === computerRndm) {
+        myTies++;
+        alert("this was a tie!!!, " + myTies + " ties so far");        
+    } 
+
+    else if ((userOptions === "r" && computerRndm === "s") ||
+            (userOptions === "p" && computerRndm === "r") ||
+            (userOptions === "s" && computerRndm === "p")) {
+                myWins++;
+                alert("this was a win!!!, " + myWins + " wins so far"); 
+            }
+
+            else{
+                myLosses++;
+                alert("this is a loss!!!, " + myLosses + " losses so far"); 
+            }    
 
 }
+
+}
+//alert summary od wins and losses and draws to user
+
+alert("Game Over!!! \nWins: " + myWins + ", \nLosses: " + myLosses + ", \nTies: " + myTies);
